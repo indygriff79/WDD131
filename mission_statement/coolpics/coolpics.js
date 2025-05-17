277e05b5930ba05000f8b0f5ee62cb0df50777b4
indygriff79 <144857050+indygriff79@users.noreply.github.com>
2025-05-16 17:09:49 -0700

    added  more to cool pics

---
+++ mission_statement/coolpics/coolpics.js
@@ -0,0 +1,45 @@
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+
+function toggleMenu() {
+    menu.classList.toggle("hide");
+}
+
+function handleResize() {
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+    }
+}
+
+menuButton.addEventListener("click", toggleMenu);
+window.addEventListener("resize", handleResize);
+handleResize();
+
+const gallery = document.querySelector(".gallery");
+const viewer = document.getElementById("viewer");
+const viewerImg = document.getElementById("viewer-img");
+const closeBtn = document.querySelector(".close-viewer");
+
+gallery.addEventListener("click", (event) => {
+    const clickImg = event.target.closest("img");
+    if (clickImg) {
+        const src = clickImg.getAttribute("src");
+        const alt = clickImg.getAttribute("alt");
+        const fullSrc = src.split("-")[0] + "-full.jpeg";
+        viewerImg.setAttribute("src", fullSrc);
+        viewerImg.setAttribute("alt", alt);
+        viewer.showModal();
+    }
+});
+
+closeBtn.addEventListener("click", () =>  {
+    viewer.closest();
+});
+
+viewer.addEventListener("click", (event) => {
+    if (event.target === viewer) {
+        viewer.closest();
+    }
+});
