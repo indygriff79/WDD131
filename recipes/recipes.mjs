2d128a8175c8243f1b5b981554942535335a8964
indygriff79 <144857050+indygriff79@users.noreply.github.com>
2025-06-21 18:13:08 -0700

    made a recipe book

---
+++ recipes/recipes.mjs
@@ -0,0 +1,10 @@
+export const recipes = [
+    {
+        id: 1,
+        name: "Apple Crisp",
+        image: "images/apple-crisp.jpg",
+        rating: 4,
+        description: "This apple crisp recipe is a simple yet delicious fall dessert thats great served warm with vanilla ice cream."
+
+    }
+];
